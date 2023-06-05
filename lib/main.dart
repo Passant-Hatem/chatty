d86467afeae696005230c0151eed56e3ea68c441
Passant-Hatem <79157384+Passant-Hatem@users.noreply.github.com>
2023-06-05 03:25:58 +0300

    feat: add splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:chatty/screens/authentication_screen.dart';
 import 'package:chatty/screens/chat_screen.dart';
+import 'package:chatty/screens/splash_screen.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
@@ -28,6 +29,9 @@ class App extends StatelessWidget {
       home: StreamBuilder(
           stream: FirebaseAuth.instance.authStateChanges(),
           builder: (ctx, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const SplashScreen();
+            }
             if (snapshot.hasData) {
               return const ChatScreen();
             }
