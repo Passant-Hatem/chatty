6eabe9ce7fdea53e188e1763255b7fd1f8370d95
Passant-Hatem <79157384+Passant-Hatem@users.noreply.github.com>
2023-06-09 05:15:48 +0300

    feat: setup notification

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,11 +1,31 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 
 import '../widgets/chat_messages.dart';
 import '../widgets/send_new_message.dart';
 
-class ChatScreen extends StatelessWidget {
+class ChatScreen extends StatefulWidget {
   const ChatScreen({super.key});
+
+  @override
+  State<ChatScreen> createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+
+  void setupPushNotificaitons() async{
+    final fcm = FirebaseMessaging.instance;
+    await fcm.requestPermission();
+    final token = await fcm.getToken();
+    print(token);
+  }
+
+  @override
+  void initState(){
+    super.initState();
+    setupPushNotificaitons();
+  }
 
   @override
   Widget build(BuildContext context) {
